c034d3c888c4f8defd99e6ebc7f81670314bbf83
Nikki Goel <gupta.nikkigupta@gmail.com>
2019-11-22 13:27:00 +0530

    filter screen UI

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,8 +2,31 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
-static const routeName = '/fliters_screen';
+class FiltersScreen extends StatefulWidget {
+  static const routeName = '/fliters_screen';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _isGlutenFree = false;
+  bool _isVegan = false;
+  bool _isVegetarian = false;
+  bool _isLuctoseFree = false;
+
+  Widget _buildSwitchListTile(
+    String title,
+    String subtitle,
+    bool currentValue,
+    Function changeSwitch,
+  ) {
+    return SwitchListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        value: currentValue,
+        onChanged: changeSwitch);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +35,59 @@ static const routeName = '/fliters_screen';
         title: Text('Your Filter'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters Page'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(40),
+            child: Text(
+              'Apply filters for your meal',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          _buildSwitchListTile(
+            'Gluten Free',
+            'Only show gluten free meals',
+            _isGlutenFree,
+            (newValue) {
+              setState(() {
+                _isGlutenFree = newValue;
+              });
+            },
+          ),
+          _buildSwitchListTile(
+            'Vegan',
+            'Only show vegan meals',
+            _isVegan,
+            (newValue) {
+              setState(() {
+                _isVegan = newValue;
+              });
+            },
+          ),
+          _buildSwitchListTile(
+            'Vegetarian',
+            'Only show vegetarian meals',
+            _isVegetarian,
+            (newValue) {
+              setState(() {
+                _isVegetarian = newValue;
+              });
+            },
+          ),
+          _buildSwitchListTile(
+            'Luctose Free',
+            'Only show luctose free meals',
+            _isLuctoseFree,
+            (newValue) {
+              setState(() {
+                _isLuctoseFree = newValue;
+              });
+            },
+          ),
+        ],
       ),
     );
   }
